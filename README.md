db0413ff7675bb6a11d109a8a41db870f1a60d22
octaviolage <octavio.lage@hotmail.com>
2021-02-18 17:59:29 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# bender_muffato
+# Bender - Muffato
+bender_muffato is our audit robot for the Muffato retailer
+
+<center><img alt="bender_gif" src="https://thumbs.gfycat.com/AdorableHarmoniousCowrie-size_restricted.gif"></center>
---
+++ main.py
@@ -0,0 +1,145 @@
+import argparse
+from datetime import date, timedelta, datetime
+from google.cloud import storage
+from config_params import stores
+
+
+def exceptions_list(dt: date, store_name: str):
+  dt = datetime.strptime(dt, "%Y-%m-%d")
+  if store_name.startswith('9_') and dt < datetime(2020, 11, 5):
+    return True
+  if store_name == '94_MAX-TITO-MUFFATO' and dt < datetime(2020, 8, 3):
+    return True
+  if store_name == '99_SJRP-DAHMA' and dt < datetime(2020, 10, 12):
+    return True
+  if store_name == '100_CATANDUVA' and dt < datetime(2020, 8, 21):
+    return True
+
+
+# Compara cada nome de arquivo com os da lista passada
+def check_list(item: str, items_list: [], dt: date):
+    for item_name in items_list:
+        if item.__contains__(item_name):
+            items_list.remove(item_name)
+            return
+
+    print(f'{dt}: {item} não esta na lista de lojas!')
+
+
+# Lista todos os diretorios e arquivos do 'bucket/folder'
+def list_items(bucket: str, folder: str, filter=None):
+    storage_client = storage.Client('mediar-painel')
+    items = storage_client.list_blobs(bucket, prefix=folder + filter)
+
+    return items
+
+
+def verify(dt: date, bucket: str, folder: str, filter=''):
+    items = list_items(bucket, folder, filter)
+    # Caso para sales-raw
+    if filter.__contains__('sales-raw'):
+        items_list = stores['mediar-ftp'][folder].copy()
+    else:
+        items_list = stores[bucket][folder].copy()
+
+    for item in items:
+        if item.name.__contains__(f'{dt}'):
+            check_list(item.name, items_list, dt)
+
+    for store_name in items_list:
+        if not exceptions_list(dt, store_name):
+            print(f'{dt} falta {store_name} em {bucket}/{folder}{filter}')
+
+
+def compara_quantidade(attributes: dict, tipo: str):
+    dt = attributes['date']
+    contador1 = 0
+    contador2 = 0
+    contador3 = 0
+    storage_client = storage.Client('mediar-painel')
+    items1 = storage_client.list_blobs('mediar-ftp', prefix=f'muffato/{tipo}')
+    items2 = storage_client.list_blobs('mediar-data', prefix=f'muffato/sales-raw/{tipo}')
+    items3 = storage_client.list_blobs('mediar-data', prefix=f'muffato/transactions/')
+
+    for item in items1:
+        if item.name.__contains__(f'{dt}'):
+            contador1 += 1
+    for item in items2:
+        if item.name.__contains__(f'{dt}'):
+            contador2 += 1
+    for item in items3:
+        if item.name.__contains__(f'{dt}'):
+            contador3 += 1
+
+    if contador1 > contador2:
+        print(f'{dt} - {tipo} - ERRO: GZIP Muffato faltou {contador1 - contador2}')
+    if contador2 > contador3:
+        print(f'{dt} - {tipo} - ERRO: Sales Processing faltou {contador2 - contador3}')
+
+
+def daterange(start_date, end_date):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    for n in range(int ((end_date - start_date).days)+1):
+        yield (start_date + timedelta(n)).strftime('%Y-%m-%d')
+
+
+def is_date(x):
+    try:
+        datetime.strptime(x, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+
+def is_int(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start_date')
+    parser.add_argument('--end_date')
+    parser.add_argument('--store_ids')
+    parser.add_argument('--sleeptime')
+    args = parser.parse_args()
+    if not is_date(args['start_date']):
+        if is_int(args['start_date']):
+            args['start_date'] = (datetime.now() + timedelta(int(args['start_date']))).strftime('%Y-%m-%d')
+        else:
+            raise ValueError('--start_date is not a date or int')
+    try:
+        if not is_date(args['end_date']):
+            if is_int(args['end_date']):
+                args['end_date'] = (datetime.now() + timedelta(int(args['end_date']))).strftime('%Y-%m-%d')
+            else:
+                raise ValueError('--end_date is not a date or int')
+        assert args['end_date'] >= args['start_date'], '--end_date is not >= --start_date'
+    except:
+        args['end_date'] = args['start_date']
+
+    for date in daterange(args['start_date'], args['end_date']):
+        attributes = {'date': date}
+        if args['topic'] == 'muffato-ftp':
+              verify(attributes['date'], 'mediar-ftp', 'muffato', filter='/SALES')
+              # verify(attributes['date'], 'mediar-ftp', 'muffato', filter='/ASSORTMENT')
+        elif args['topic'] == 'muffato-raw':
+              verify(attributes['date'], 'mediar-data', 'muffato', filter='/sales-raw/SALES')
+              # verify(attributes['date'], 'mediar-data', 'muffato', filter='/sales-raw/ASSORTMENT')
+        elif args['topic'] == 'muffato-transactions':
+              verify(attributes['date'], 'mediar-data', 'muffato', filter=f'/transactions/dt={attributes["date"]}')
+        elif args['topic'] == 'muffato-assortment':
+              verify(attributes['date'], 'mediar-data', 'muffato', filter=f'/assortment/dt={attributes["date"]}')
+        elif args['topic'] == 'sva-ftp':
+              verify(attributes['date'], 'mediar-ftp', 'sva')
+        elif args['topic'] == 'sva-raw':
+              verify(attributes['date'], 'mediar-data', 'sva', filter='/video-raw')
+        elif args['topic'] == 'sva-metrics':
+              verify(attributes['date'], 'mediar-data', 'sva', filter='/video-metrics')
+        elif args['topic'] == 'sva-filtered':
+              verify(attributes['date'], 'mediar-data', 'sva', filter='/video-filtered')
+        elif args['topic'] == 'muffato-compara':
+              compara_quantidade(attributes, 'SALES')
